7bce787d3e6fcb1159dedb40ccaff7c34f77db73
Neil Dingeldein <ndingeldein@gmail.com>
2013-03-27 02:27:53 -0500

    test of how it displays changes in committed file

--- index.php
+++ index.php
@@ -159,51 +159,7 @@
 
 <script>
 
-(function(){
 
-	var opts = {
-		lines: 11, // The number of lines to draw
-		length: 21, // The length of each line
-		width: 6, // The line thickness
-		radius: 31, // The radius of the inner circle
-		corners: 1, // Corner roundness (0..1)
-		rotate: 0, // The rotation offset
-		color: '#666', // #rgb or #rrggbb
-		speed: 1, // Rounds per second
-		trail: 60, // Afterglow percentage
-		shadow: false, // Whether to render a shadow
-		hwaccel: false, // Whether to use hardware acceleration
-		className: 'spinner', // The CSS class to assign to the spinner
-		zIndex: 2e9, // The z-index (defaults to 2000000000)
-		top: 'auto', // Top position relative to parent in px
-		left: 'auto' // Left position relative to parent in px
-	};
-
-	var target = document.getElementById('loader');
-	var spinner = new Spinner(opts).spin(target);
-
-	jQuery(document).ready(function($) {
-		$('.bg').hide();
-		$('.content-wrapper').hide();
-		$('.footer-wrapper').hide();
-	});
-	
-	
-	jQuery(window).load(function() {
-		
-		$('body').imagesLoaded(function(){
-			$('.bg').fadeIn({'easing':'easeInOutSine', 'duration':1000});
-			$('.footer-wrapper').fadeIn({'easing':'easeInOutSine', 'duration':1000});
-			$('.content-wrapper').delay(500).fadeIn({'easing':'easeInOutSine', 'duration':1000});
-			$('#loader').fadeOut({'easing':'easeInOutSine', 'duration':500, 'complete':function(){
-				spinner.stop();
-			}});
-		});
-	});
-	
-
-
-})();
 
 </script>
 
